4b6ef8769da4791fb668b2b7618a6074cd72db32
red-hat01 <carefreelakshay97@gmail.com>
2020-05-22 18:29:52 +0530

    added routes

--- stream-n-learn-server/index.js
+++ stream-n-learn-server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
